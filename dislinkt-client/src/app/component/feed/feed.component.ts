59dce1544522f5ff1783a9d83c7258788684d1dd
marija.petrovic <marijap@protonmail.com>
2022-04-27 00:21:09 +0200

    feat(05): add input for comments

--- dislinkt-client/src/app/component/feed/feed.component.ts
+++ dislinkt-client/src/app/component/feed/feed.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  public canComment: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showComentInput(): void {
+    this.canComment = !this.canComment;
   }
 
 }
