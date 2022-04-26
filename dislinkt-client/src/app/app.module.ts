f7abb139d6476504d57df3a7daf49312aeedaa5d
anagrahovac <grahovac.ana99@gmail.com>
2022-04-26 13:32:19 +0200

    Squashed commit of the following:
    commit 3b9720366fdee2e8220ddaee5c21fe9a5ab38ef0
    Author: anagrahovac <grahovac.ana99@gmail.com>
    Date:   Tue Apr 26 13:31:02 2022 +0200
    
        feat(02): add login page
    
    commit 75c9d6cb5d003d1d8156a434e536e8459631cd55
    Author: anagrahovac <grahovac.ana99@gmail.com>
    Date:   Tue Apr 26 13:14:50 2022 +0200
    
        feat(02): add components for login and registration

--- dislinkt-client/src/app/app.module.ts
+++ dislinkt-client/src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './component/registration/registration.component';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
