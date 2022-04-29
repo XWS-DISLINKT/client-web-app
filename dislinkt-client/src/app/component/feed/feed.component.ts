0be904702f37e6ddaea6bcb179ee854fb37dab33
anagrahovac <grahovac.ana99@gmail.com>
2022-04-29 19:39:04 +0200

    feat(06): update feed page

--- dislinkt-client/src/app/component/feed/feed.component.ts
+++ dislinkt-client/src/app/component/feed/feed.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FeedComponent implements OnInit {
 
   public canComment: boolean = false;
+  public isProfileOwner: boolean = true;
 
   constructor() { }
 
